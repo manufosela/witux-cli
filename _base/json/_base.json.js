477dd50d2d5de24f98a146ec4d4f8eb3b69bd847
Manuel Juan Fosela Águila <manufosela@gmail.com>
2020-10-13 20:59:23 +0200

    add json file

---
+++ _base/json/_base.json.js
@@ -0,0 +1,24 @@
+import navigationMenu from './navigationMenu.json.js';
+import banners from './banner.json.js';
+import footer from './footer.json.js';
+
+export default {
+  'Homepage': {
+    'sectionName': 'ventures'
+  },
+  'navigationMenu': navigationMenu,
+  'banners': banners.banners,
+  'footer': footer,
+  '_base': {
+    'en': {
+      'fake': {
+        'title': 'TITLE _base 1'
+      }
+    },
+    'es': {
+      'fake': {
+        'title': 'TITULO _base 1'
+      }
+    }
+  }
+};
