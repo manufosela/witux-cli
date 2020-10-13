0bb1bf7016dcf0e649f89e778abb5db8e8ce582a
Manuel Juan Fosela Águila <manufosela@gmail.com>
2020-10-13 23:28:41 +0200

    fixed eslint errors

--- _base/js/tpl/_base.tpl.js
+++ _base/js/tpl/_base.tpl.js
@@ -1,5 +1,5 @@
 // Datos y Plantillas
-import {HTMLbody} from '../pages/_base.html.mjs';
+import { HTMLbody } from '../pages/_base.html.mjs';
 window.onload = function() {
   const body = document.getElementsByTagName('body');
   body[0].innerHTML = HTMLbody;
