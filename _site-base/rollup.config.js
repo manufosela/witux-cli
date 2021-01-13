8fea34a010010625fc3d145fefb41710afa980c7
Manuel Juan Fosela Águila <manufosela@gmail.com>
2021-01-14 00:28:55 +0100

    fixed packages in rollup.config base

--- _site-base/rollup.config.js
+++ _site-base/rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 import copy from 'rollup-plugin-copy';
 import multiInput from 'rollup-plugin-multi-input';
 import {
